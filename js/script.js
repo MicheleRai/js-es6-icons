a8445d1845f1dea6bfb2f559093be36d8682a8c4
MicheleRai <rainonemichele1999@gmail.com>
2022-11-09 14:10:42 +0100

    first try

--- js/script.js
+++ js/script.js
@@ -19,112 +19,125 @@ const arrIcon = [
 		name: 'cat',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'orange'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa-solid',
 		color: 'blue'
 	}
 ];
+
+const eleIconContainer = document.querySelector('.icon-container')
+
+for (let i = 0; i < arrIcon.length; i++) {
+	const objIcon = arrIcon[i];
+	const eleIcon = document.createElement('div');
+	eleIcon.classList.add('box-icon'); 
+	eleIcon.innerHTML = `
+        <i class="${objIcon.family} ${objIcon.prefix}${objIcon.name}"></i>
+		<h2>${objIcon.name}</h2>
+	`;
+	eleIconContainer.append(eleIcon);
+}
